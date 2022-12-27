786b0e30bf1d4f44d8cc5efc921df711f9235393
Tiffany DURR <103035560+TiffanyDURR@users.noreply.github.com>
2022-12-27 05:17:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,3 +3,364 @@ const goplay = (lame) => {
   audio.src = lame + ".mp3"; //on attribue une lettre au nom du fichier et on considere que c'est une key
   audio.play();
 };
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === "q") goplay(e.key);
+  if (e.key === "a") goplay(e.key);
+  if (e.key === "w") goplay(e.key);
+  if (e.key === "s") goplay(e.key);
+  if (e.key === "e") goplay(e.key);
+  if (e.key === "d") goplay(e.key);
+  if (e.key === "r") goplay(e.key);
+  if (e.key === "f") goplay(e.key);
+  if (e.key === "g") goplay(e.key);
+  if (e.key === "y") goplay(e.key);
+  if (e.key === "j") goplay(e.key);
+  if (e.key === "u") goplay(e.key);
+  if (e.key === "i") goplay(e.key);
+  if (e.key === "k") goplay(e.key);
+  if (e.key === "l") goplay(e.key);
+  if (e.key === "o") goplay(e.key);
+  if (e.key === "p") goplay(e.key);
+});
+
+const lame001 = document.getElementById("lame001");
+const lame002 = document.getElementById("lame002");
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === "q") {
+    lame001.style.background = "rgb(255, 255, 255, 0.5)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+  if (e.key === "a") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0.5)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+  if (e.key === "w") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0.5)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+  if (e.key === "s") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0.5)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+  if (e.key === "e") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0.5)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "d") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0.5)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "r") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0.5)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "f") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0.5)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "g") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0.5)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "y") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0.5)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "j") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0.5)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "u") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0.5)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "i") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0.5)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "k") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0.5)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "l") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0.5)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "o") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0.5)";
+    lame017.style.background = "rgb(255, 255, 255, 0)";
+  }
+
+  if (e.key === "p") {
+    lame001.style.background = "rgb(255, 255, 255, 0)";
+    lame002.style.background = "rgb(255, 255, 255, 0)";
+    lame003.style.background = "rgb(255, 255, 255, 0)";
+    lame004.style.background = "rgb(255, 255, 255, 0)";
+    lame005.style.background = "rgb(255, 255, 255, 0)";
+    lame006.style.background = "rgb(255, 255, 255, 0)";
+    lame007.style.background = "rgb(255, 255, 255, 0)";
+    lame008.style.background = "rgb(255, 255, 255, 0)";
+    lame009.style.background = "rgb(255, 255, 255, 0)";
+    lame010.style.background = "rgb(255, 255, 255, 0)";
+    lame011.style.background = "rgb(255, 255, 255, 0)";
+    lame012.style.background = "rgb(255, 255, 255, 0)";
+    lame013.style.background = "rgb(255, 255, 255, 0)";
+    lame014.style.background = "rgb(255, 255, 255, 0)";
+    lame015.style.background = "rgb(255, 255, 255, 0)";
+    lame016.style.background = "rgb(255, 255, 255, 0)";
+    lame017.style.background = "rgb(255, 255, 255, 0.5)";
+  }
+});
